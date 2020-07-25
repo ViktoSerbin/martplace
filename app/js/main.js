00e9bcf4388ec903702b9fc2e9bdaebf904c8acc
ViktoSerbin <taur1721@gmail.com>
2020-07-26 00:29:39 +0300

    added product-slider and products

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,21 @@
-$(function() {
+$(function () {
+
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "15px",
+        ratedFill: "#ffc000",
+        readOnly: true,
+    });
+
+    $(".product-slider__inner").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="images/chevron-left.png" alt="Previous arrow"></button>',
+        nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="images/chevron-right.png" alt="Next arrow"></button>',
+    });
 
 
 
+    var mixer = mixitup('.products__inner-box');
 
 });
