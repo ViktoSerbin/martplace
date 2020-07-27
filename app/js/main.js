46fe73c85b7551f602f8ce0241c973e1351c94b7
ViktoSerbin <taur1721@gmail.com>
2020-07-27 22:43:51 +0300

    added followers caunter benefits

--- app/js/main.js
+++ app/js/main.js
@@ -14,6 +14,14 @@ $(function () {
         nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="images/chevron-right.png" alt="Next arrow"></button>',
     });
 
+    $(".followerslist__inner").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: false,
+        prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="images/chevron-left.png" alt="Previous arrow"></button>',
+        nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="images/chevron-right.png" alt="Next arrow"></button>',
+    });
+
 
 
     var mixer = mixitup('.products__inner-box');
