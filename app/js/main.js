b67cd98fe911a359991be6ffa249a643fb3874f2
ViktoSerbin <taur1721@gmail.com>
2020-08-04 21:52:49 +0300

    added product-page-v1

--- app/js/main.js
+++ app/js/main.js
@@ -22,6 +22,29 @@ $(function () {
         nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="images/chevron-right.png" alt="Next arrow"></button>',
     });
 
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 300,
+        prefix: "$"
+    });
+
+    $('input[type="checkbox"], select').styler();
+
+    $('.list-btn').on('click', function () {
+        // $('.product__item').addClass('list');
+        $('.list-btn').addClass('active');
+        $('.grid-btn').removeClass('active')
+    });
+
+    $('.grid-btn').on('click', function () {
+        // $('.product__item').removeClass('list');
+        $('.grid-btn').addClass('active');
+        $('.list-btn').removeClass('active');
+    });
+
 
 
     var mixer = mixitup('.products__inner-box');
